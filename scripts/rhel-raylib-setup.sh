ea76250261ecd0eebac954264bea1266e95562a2
underpostnet <fcoverdugoa@underpost.net>
2025-11-24 10:16:16 -0300

    Update rhel-raylib-setup.sh

--- scripts/rhel-raylib-setup.sh
+++ scripts/rhel-raylib-setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Setup raylib for Emscripten
 
-# cd $HOME
-cd lib
+LIB_DIR=lib/raylib
 
-git clone https://github.com/raysan5/raylib.git
+mkdir -p $LIB_DIR
 
-cd raylib
+git subrepo clone https://github.com/raysan5/raylib.git $LIB_DIR
+
+cd $LIB_DIR
 
 emcmake cmake . -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release
 emmake make -j$(nproc)
