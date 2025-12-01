1c9cbdda202e7ff9e2070aa3c3dd73a71e10f6f7
underpostnet <fcoverdugoa@underpost.net>
2025-12-01 10:32:59 -0300

    Nuke python

--- config.py
+++
@@ -1,9 +0,0 @@
-WS_URL = "wss://server.cyberiaonline.com/ws"
-# WS_URL = "ws://localhost:8080/ws"
-API_BASE_URL = "https://server.cyberiaonline.com/api/v1"
-# API_BASE_URL = "http://localhost:8080/api/v1"
-ASSETS_BASE_URL = "https://server.cyberiaonline.com/assets"
-# ASSETS_BASE_URL = "http://localhost:8080/assets"
-
-# Ghost item ID - shown when player/bot is dead
-GHOST_ITEM_ID = "ghost"
