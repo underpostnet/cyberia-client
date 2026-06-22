e4d7ab679a47176e14276f7378f9fa517cfea8e0
futureapricot <2019446+futureapricot@users.noreply.github.com>
2026-06-21 23:52:30 -0400

    Shrinks Dockerfile

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-# Deploy Cyberia Client via GitHub Actions workflow dispatch
-set -e
-
-cd /home/dd/engine/cyberia-client
-underpost push . underpostnet/cyberia-client
-gh workflow run cyberia-client.cd.yml -R underpostnet/cyberia-client -f job=deploy
